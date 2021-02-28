7e7b54e9af82a76253ace7e289c2fc4065574d01
Marcela F Fortis <marmarmarcelacela@gmail.com>
2021-02-28 21:46:47 +0100

    rename tasks, set deadline, set project functionalities

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,43 @@
+import java.util.HashMap;
+import java.util.Date;
+
+
 public class Task {
+    private String name;
+    private String project;
+    private Date deadline;
+	private boolean isDone;
+
+
+   public String getName() {
+       return name;
+   }
+
+   public void setName() {
+       this.name = name;
+    }
+
+   public Date getDeadline(){
+       return deadline;
+   }
+    public void setDeadline(Date deadline){
+       this.deadline = deadline;
+    }
+
+    public String getProject(){
+       return project;
+    }
+    public void setProject(String project){
+       this.project = project;
+
+    }
+
+    public boolean isDone() {
+       return isDone;
+    }
+    public void setDone(boolean isDone){
+       this.isDone =isDone;
+    }
+
+
 }
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,2 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+
+
 public class ToDoList {
+    String project;
+    List<Task> tasks = new ArrayList<>();
+
+    public ToDoList(String projectName){
+        project= projectName;
+
+    }
+
+
 }
